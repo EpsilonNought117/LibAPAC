184e74ea282174c83cc65f1281c2b40e0f2abda6
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2024-08-28 16:15:54 +0530

    initialization, grow and shrink changed to be multiple of 4 in size

--- apz/platform_independent/apz_init/apz_init.c
+++ apz/platform_independent/apz_init/apz_init.c
@@ -2,9 +2,11 @@
 
 libapac_err apz_init(apz_t* result, size_t init_size)
 {
-	assert(result && init_size);
+	assert(result && init_size && ((init_size >> 3) < ULLONG_MAX));
 
-	result->num_array = malloc_ptr(sizeof(apz64) * init_size);
+	apz64 alloc_size = (((apz64)8 * init_size) >> 2) << 2;
+
+	result->num_array = malloc_ptr(alloc_size);
 
 	if (!result->num_array)
 	{
@@ -13,7 +15,7 @@ libapac_err apz_init(apz_t* result, size_t init_size)
 	}
 
 	result->seg_alloc = init_size;
-	result->num_array = memset(result->num_array, result->seg_alloc, sizeof(apz64));
+	result->num_array = memset(result->num_array, 0, result->seg_alloc * sizeof(apz64));
 
 	result->seg_in_use = 0;
 	result->is_negative = APZ_NOT_IN_USE;
@@ -23,18 +25,21 @@ libapac_err apz_init(apz_t* result, size_t init_size)
 
 libapac_err apz_init_u64(apz_t* result, size_t init_size, uint64_t init_value)
 {
-	assert(result && init_size);
+	assert(result && init_size && ((init_size >> 3) < ULLONG_MAX));
 
-	result->num_array = (apz64)malloc_ptr(init_size * sizeof(apz64));
+	apz64 alloc_size = (((apz64)8 * init_size) >> 2) << 2;
+
+	result->num_array = malloc_ptr(alloc_size);
 
 	if (!result->num_array)
 	{
 		fprintf(stderr, "Memory allocation failed in 'apz_init_u64'!");
 		return LIBAPAC_OOM;
+
 	}
 
 	result->seg_alloc = init_size;
-	result->num_array = memset(result->num_array, result->seg_alloc, sizeof(apz64));
+	result->num_array = memset(result->num_array, 0, result->seg_alloc * sizeof(apz64));
 
 	result->num_array[0] = init_value;
 	result->seg_in_use = 1;
@@ -45,9 +50,11 @@ libapac_err apz_init_u64(apz_t* result, size_t init_size, uint64_t init_value)
 
 libapac_err apz_init_i64(apz_t* result, size_t init_size, int64_t init_value)
 {
-	assert(result && init_size && init_value);
+	assert(result && init_size && ((init_size >> 3) < ULLONG_MAX));
 
-	result->num_array = (apz64)malloc_ptr(init_size * sizeof(apz64));
+	apz64 alloc_size = (((apz64)8 * init_size) >> 2) << 2;
+
+	result->num_array = malloc_ptr(alloc_size);
 
 	if (!result->num_array)
 	{
@@ -56,7 +63,7 @@ libapac_err apz_init_i64(apz_t* result, size_t init_size, int64_t init_value)
 	}
 
 	result->seg_alloc = init_size;
-	result->num_array = memset(result->num_array, result->seg_alloc, sizeof(apz64));
+	result->num_array = memset(result->num_array, 0, result->seg_alloc * sizeof(apz64));
 
 	apz64 abs_value = (init_value ^ (init_value >> 63)) - (init_value >> 63);   // calculate absolute value of init_value
 
@@ -69,8 +76,10 @@ libapac_err apz_init_i64(apz_t* result, size_t init_size, int64_t init_value)
 
 libapac_err apz_grow(apz_t* result, size_t new_size)
 {
-	assert(result && (new_size > result->seg_alloc));
+	assert(result && (new_size > result->seg_alloc) && ((new_size >> 3) < ULLONG_MAX));
 	
+	new_size = (new_size >> 2) << 2;
+
 	apz64* temp_mem = realloc_ptr(result->num_array, new_size * sizeof(apz64));
 
 	if (!temp_mem)
@@ -78,6 +87,10 @@ libapac_err apz_grow(apz_t* result, size_t new_size)
 		fprintf(stderr, "Memory allocation failed in 'apz_grow'!");
 		return LIBAPAC_OOM;
 	}
+
+	apz64* extended_addr = temp_mem + result->seg_alloc * sizeof(apz64);
+
+	extended_addr = memset(extended_addr, 0, new_size - result->seg_alloc);
 
 	result->num_array = temp_mem;
 	result->seg_alloc = new_size;
@@ -90,7 +103,9 @@ libapac_err apz_shrink_fit(apz_t* result)
 {
 	assert(result && result->num_array);
 
-	apz64* temp_mem = realloc_ptr(result->num_array, sizeof(apz64) * result->seg_in_use);
+	apz64 reduced_size = (result->seg_in_use >> 2) << 2;
+
+	apz64* temp_mem = realloc_ptr(result->num_array, sizeof(apz64) * reduced_size);
 
 	if (!temp_mem)
 	{
@@ -99,7 +114,8 @@ libapac_err apz_shrink_fit(apz_t* result)
 	}
 
 	result->num_array = temp_mem;
-	result->seg_alloc = result->seg_in_use;
+	result->seg_alloc = reduced_size;
+	result->seg_in_use = reduced_size;
 	temp_mem = NULL;
 
 	return LIBAPAC_OKAY;
