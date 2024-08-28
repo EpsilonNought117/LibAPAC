6790a1c79af05339100fa889588a4c25305d3f64
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2024-08-28 17:41:15 +0530

    Changed directory structure

--- LibAPAC/LibAPAC.h
+++ LibAPAC/LibAPAC.h
@@ -1,14 +1,14 @@
 #ifndef LIBAPAC
 #define LIBAPAC
 
+#include "LibAPAC_erros.h"
+#include "LibAPAC_memory_allocator/LibAPAC_memory_alloc.h"
+
 #include "apz/apz_def.h"
 #include "apz/platform_independent/apz_init/apz_init.h"
 #include "apz/platform_independent/apz_assign/apz_assign.h"
 #include "apz/platform_independent/apz_op_result_size/apz_op_limit.h"
 
 #include "apz/AMD64/apz_basic_arithmetic_amd64/apz_basic_arithmetic_amd64.h"
-
-
-#include "LibAPAC_memory_allocator/LibAPAC_memory_alloc.h"
 
 #endif
