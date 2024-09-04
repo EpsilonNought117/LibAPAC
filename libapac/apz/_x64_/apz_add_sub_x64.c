b6a8b2a680ce41aeb993fcce97a15b05a4945762
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2024-09-04 07:37:59 +0530

    fixed apz-add_sub_x64.c file for some errors and reduced function overhead

--- libapac/apz/_x64_/apz_add_sub_x64.c
+++ libapac/apz/_x64_/apz_add_sub_x64.c
@@ -1,44 +1,42 @@
 #include "apz_add_sub_x64.h"
 
-void apz_abs_add_x64(apz_t *result, const apz_t *op1, const apz_t *op2)
+void apz_abs_add_x64(apz_t *result, const apz_t *max_elem, const apz_t *min_elem)
 {
-    assert(result && op1 && op2);
-    assert(result->num_array && op1->num_array && op2->num_array);
+    assert(result && max_elem && min_elem);
+    assert(result->num_array && max_elem->num_array && min_elem->num_array);
 
-    apz_t *min; // points to the big int with lesser number of used limbs
-    apz_t *max; // points to the big int with greater number of used limbs
+    // assumption made by function
 
-    if (op1->seg_in_use > op2->seg_in_use)
-    {
-        max = op1;
-        min = op2;
-    }
-    else
-    {
-        max = op2;
-        min = op1;
-    }
+    assert(max_elem->seg_in_use >= min_elem->seg_in_use);
 
     uint64_t counter = 0; // counter to keep track of number of segments processed
 
-    uint8_t carry = 0; // carry variable for adcx and adox respectively
+    uint8_t carry = 0; // carry variable for carry propagation
 
-    apz64 *temp_res = result->num_array, *temp_max = max->num_array, *temp_min = min->num_array;
+    apz64 *temp_res = result->num_array, *temp_max = max_elem->num_array, *temp_min = min_elem->num_array;
 
     // process min->seg_in_use - 1 limbs
 
-    while (counter < min->seg_in_use)
+    while (counter < min_elem->seg_in_use)
     {
         carry = _addcarry_u64(carry, temp_max[counter], temp_min[counter], temp_res + counter);
         counter++;
+
+        /*
+            TEST ASM WITH ADCX AND ADOX ALONG WITH LOOP UNROLLING
+        */
     }
 
     // process remaining limbs of max along with carry
 
-    while (counter < max->seg_in_use)
+    while (counter < max_elem->seg_in_use)
     {
         carry = _addcarry_u64(carry, temp_max[counter], 0, temp_res + counter);
         counter++;
+
+        /*
+            TEST ASM WITH ADCX AND ADOX ALONG WITH LOOP UNROLLING, MAYBE LOOP UNROLLING WILL SUFFICE
+        */
     }
 
     // check if carry is still left, if so then add it to final limb
@@ -48,59 +46,49 @@ void apz_abs_add_x64(apz_t *result, const apz_t *op1, const apz_t *op2)
         temp_res[counter] += carry;
     }
 
-    result->seg_in_use = max->seg_in_use + (carry & 1);  
-    
-    /* (carry & 1) ensures limb count is increased if carry occured on last limb */ 
+    result->seg_in_use = max_elem->seg_in_use + (carry & 1);
+    result->is_negative = APZ_ZPOS;
+
+    /* (carry & 1) ensures limb count is increased if carry occured on last limb */
 
     return;
 }
 
-void apz_abs_sub_x64(apz_t *result, const apz_t *op1, const apz_t *op2)
+void apz_abs_sub_x64(apz_t *result, const apz_t *max_elem, const apz_t *min_elem)
 {
-    assert(result && op1 && op2);
-    assert(result->num_array && op1->num_array && op2->num_array);
+    assert(result && max_elem && min_elem);
+    assert(result->num_array && max_elem->num_array && min_elem->num_array);
 
-    // to produce correct results, assert following
-
-    assert((op1->seg_in_use > op2->seg_in_use));                    
-    assert((op1->seg_in_use == op2->seg_in_use) && (op1->num_array[op1->seg_in_use - 1] > op2->num_array[op2->seg_in_use - 1]));
-
-    apz_t *max = op1;
-    apz_t *min = op2;
+    /*
+        NO CHECKS ARE PERFORMED HERE WHETHER abs(max_elem) > abs(min_elem)
+        NEEDS TO BE CHECKED BY CALLER OR USE HIGHER LEVEL FUNCTIONS
+    */
 
     uint64_t counter = 0; // counter to keep track of number of segments processed
 
-    uint8_t borrow = 0; // borrow variable to keep track of
+    uint8_t borrow = 0; // borrow variable for borrow propagation
 
-    apz64 *temp_res = result->num_array, *temp_max = max->num_array, *temp_min = min->num_array;
+    apz64 *temp_res = result->num_array, *temp_max = max_elem->num_array, *temp_min = min_elem->num_array;
 
     // process min->seg_in_use - 1 limbs
 
-    while (counter < min->seg_in_use)
+    while (counter < min_elem->seg_in_use)
     {
-        borrow = _subborrow_u64(borrow, temp_max[counter], temp_min[counter], temp_res + counter);
+        borrow = _subborrow_u64(borrow, temp_max[counter], temp_min[counter], temp_res + counter);  // possibly loop unroll
         counter++;
     }
 
-    // process last borrow
+    // process remaining limbs
 
-    if (max->seg_in_use > min->seg_in_use)
+    while (counter < max_elem->seg_in_use)
     {
-        temp_res[counter] = temp_max[counter]; 
-        temp_res[counter] -= borrow;
-        counter++;
-    }
-
-    // copy the remaining limbs
-
-    while (counter < max->seg_in_use)
-    {
-        temp_res[counter] = temp_max[counter];
+        borrow = _subborrow_u64(borrow, temp_max[counter], 0, temp_res + counter);                 // possibly loop unroll
         counter++;
     }
 
     // change size to max->seg_in_use
 
-    result->seg_in_use = max->seg_in_use;
-    return; 
+    result->seg_in_use = max_elem->seg_in_use;
+    result->is_negative = APZ_ZPOS;
+    return;
 }
